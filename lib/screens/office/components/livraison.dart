3c599ecb0c7b7822e698e18815b83188f9173907
Moubarak321 <benmoumass@gmail.com>
2024-01-17 12:01:55 +0100

    revue d'anciens commit

--- lib/screens/office/components/livraison.dart
+++ lib/screens/office/components/livraison.dart
@@ -167,3 +167,155 @@ class _DeliveryInfoWidgetState extends State<DeliveryInfoWidget> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:intl_phone_field/intl_phone_field.dart';
+
+// // ignore: must_be_immutable
+// class DeliveryInfoWidget extends StatelessWidget {
+//   final GlobalKey<FormState> formKey;
+//   String? deliveryAddress;
+//   int? deliveryNumero;
+//   final Function(String, int) updateDeliveryInfo;
+
+//   DeliveryInfoWidget({
+//     required this.formKey,
+//     required this.deliveryAddress,
+//     required this.deliveryNumero,
+//     required this.updateDeliveryInfo,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: <Widget>[
+//         Text(
+//           'Identifiant',
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             fontWeight: FontWeight.bold,
+//             color: Colors.orange,
+//           ),
+//         ),
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(
+//               color: Colors.lightBlue,
+//             ),
+//             borderRadius: BorderRadius.circular(8.0),
+//           ),
+//           child: TextFormField(
+//             decoration: InputDecoration(
+//               labelText: 'Prénom',
+//               prefixIcon: Icon(Icons.person_2_rounded),
+//             ),
+//             onChanged: (value) {
+//               // Appeler la fonction pour mettre à jour les données
+//               updateDeliveryInfo(value, deliveryNumero ?? 0);
+//             },
+//             validator: (value) {
+//               if (value == null || value.isEmpty) {
+//                 return 'Ce champ est requis pour une bonne orientation';
+//               }
+//               return null;
+//             },
+//           ),
+//         ),
+//         SizedBox(height: 20.0),
+
+//         Text(
+//           'Rendre à',
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             fontWeight: FontWeight.bold,
+//             color: Colors.orange,
+//           ),
+//         ),
+       
+//         IntlPhoneField(
+//           key: Key('phoneFieldKey'), 
+//           flagsButtonPadding: const EdgeInsets.all(5),
+//           dropdownIconPosition: IconPosition.trailing,
+//           initialCountryCode: 'BJ',
+//           decoration: const InputDecoration(
+//             labelText: 'Numéro',
+//             labelStyle: TextStyle(color: Color.fromRGBO(250, 153, 78, 1)),
+//             filled: true,
+//             fillColor: Colors.white,
+//             alignLabelWithHint: true,
+//             border: OutlineInputBorder(
+//               borderSide: BorderSide(),
+//               borderRadius: BorderRadius.all(Radius.circular(35)),
+//             ),
+//           ),
+//           keyboardType: TextInputType.number,
+//           onChanged: (value) {
+//             // Extraire le numéro de téléphone
+//             final phoneNumber = value.completeNumber;
+           
+//               // Appeler la fonction pour mettre à jour les données
+//             updateDeliveryInfo(deliveryAddress ?? '', int.parse(phoneNumber));
+           
+//           },
+//           validator: (value) {
+//             if (value == null) {
+//               return 'Il est important de préciser un numéro de contact';
+//             }
+//             return null;
+//           },
+//         ),
+//       ],
+//     );
+//   }
+// }
--- lib/screens/office/components/recuperation.dart
+++ lib/screens/office/components/recuperation.dart
@@ -173,3 +173,232 @@ class _PickupInfoWidgetState extends State<PickupInfoWidget> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=========================================ancien code =========================================
+// import 'package:flutter/material.dart';
+// import 'package:intl_phone_field/intl_phone_field.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// // ignore: must_be_immutable
+// class PickupInfoWidget extends StatefulWidget {
+//   final GlobalKey<FormState> formKey;
+//   String? pickupAddress;
+//   int? pickupNumero;
+//   final Function(String, int) updatePickupInfo;
+
+//   PickupInfoWidget({
+//     required this.formKey,
+//     required this.pickupAddress,
+//     required this.pickupNumero,
+//     required this.updatePickupInfo,
+//   });
+
+//   @override
+//   _PickupInfoWidgetState createState() => _PickupInfoWidgetState();
+// }
+
+// class _PickupInfoWidgetState extends State<PickupInfoWidget> {
+//   List<String> addressList = [];
+//   String? selectedAddress;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchDeliveryAddresses(); // Appel de la fonction au démarrage de la page
+//   }
+
+//   void fetchDeliveryAddresses() async {
+//   try {
+//     // Récupérer les données depuis Firestore
+//     DocumentSnapshot zoneSnapshot = await FirebaseFirestore.instance
+//         .collection('administrateur')
+//         .doc('zone')
+//         .get();
+
+//     // Vérifier si le document existe et s'il contient la clé 'livraison'
+//     if (zoneSnapshot.exists) {
+//       Map<String, dynamic>? data = zoneSnapshot.data() as Map<String, dynamic>?;
+//       if (data != null && data.containsKey('livraison')) {
+//         List<dynamic> livraisonList = data['livraison'];
+//         setState(() {
+//           addressList =
+//               List<String>.from(livraisonList); 
+//           selectedAddress = addressList.isNotEmpty ? addressList.first : null;
+//         });
+//       }
+//     }
+//   } catch (e) {
+//     print('Erreur lors de la récupération des adresses de livraison : $e');
+//     // Gérer l'erreur selon vos besoins
+//   }
+// }
+
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.start,
+//       children: <Widget>[
+//         Text(
+//           "Zone d'échange",
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             fontWeight: FontWeight.bold,
+//             color: Colors.orange,
+//           ),
+//         ),
+//         Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(
+//               color: Colors.lightBlue,
+//             ),
+//             borderRadius: BorderRadius.circular(8.0),
+//           ),
+//           child: DropdownButtonFormField<String>(
+//             decoration: InputDecoration(
+//               labelText: 'Zone',
+//               prefixIcon: Icon(Icons.location_on),
+//             ),
+//             value: selectedAddress ?? addressList.first,
+//             items: addressList.map((String value) {
+//               return DropdownMenuItem<String>(
+//                 value: value,
+//                 child: Text(value),
+//               );
+//             }).toList(),
+//             onChanged: (String? newValue) {
+//               // Mettre à jour la valeur sélectionnée
+//               setState(() {
+//                 selectedAddress = newValue;
+//               });
+//               widget.updatePickupInfo(newValue ?? '', widget.pickupNumero ?? 0);
+//             },
+//             validator: (value) {
+//               if (value == null || value.isEmpty) {
+//                 return 'Il est important de préciser une adresse de récupération';
+//               }
+//               return null;
+//             },
+//           ),
+//         ),
+//         SizedBox(height: 20.0),
+//         Text(
+//           'Prendre chez',
+//           style: TextStyle(
+//             fontSize: 20.0,
+//             fontWeight: FontWeight.bold,
+//             color: Colors.orange,
+//           ),
+//         ),
+//         IntlPhoneField(
+//           key: Key('phoneFieldKey'),
+//           flagsButtonPadding: const EdgeInsets.all(5),
+//           dropdownIconPosition: IconPosition.trailing,
+//           initialCountryCode: 'BJ',
+//           decoration: const InputDecoration(
+//             labelText: 'Numéro',
+//             labelStyle: TextStyle(color: Color.fromRGBO(250, 153, 78, 1)),
+//             filled: true,
+//             fillColor: Colors.white,
+//             alignLabelWithHint: true,
+//             border: OutlineInputBorder(
+//               borderSide: BorderSide(),
+//               borderRadius: BorderRadius.all(Radius.circular(35)),
+//             ),
+//           ),
+//           keyboardType: TextInputType.phone,
+//           onChanged: (value) {
+//             // Extraire le numéro de téléphone
+//             final phoneNumber = value.completeNumber;
+//             // Appeler la fonction pour mettre à jour les données
+//             widget.updatePickupInfo(
+//                 widget.pickupAddress ?? '', int.tryParse(phoneNumber) ?? 0);
+//           },
+//           validator: (value) {
+//             if (value == null) {
+//               return 'Il est important de préciser un numéro de contact';
+//             }
+//             return null;
+//           },
+//         ),
+//       ],
+//     );
+//   }
+// }
